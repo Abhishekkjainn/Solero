d9181cb161a01a28cab959e5641b635c9f6ccd17
abhishekkjainn <jainabhishek1904@gmail.com>
2024-06-14 00:57:23 +0530

    Added the Dimensions Page

--- src/pages/Page6.jsx
+++ src/pages/Page6.jsx
@@ -1,3 +1,32 @@
 export default function Page6() {
-  return <div className="page6 page"></div>;
+  return (
+    <div className="page6 page">
+      <div className="page2head pg6head">
+        <div className="circle"></div> &nbsp; Explore Dimesions According to you
+      </div>
+      <div className="dimensionsdiv">
+        <div className="dimensionbox dimensionbox1">
+          <div className="dimcontent">800 x 800 mm</div>
+        </div>
+        <div className="dimensionbox dimensionbox2">
+          <div className="dimcontent">1200 x 1200 mm</div>
+        </div>
+        <div className="dimensionbox dimensionbox3">
+          <div className="dimcontent">1200 x 2400 mm</div>
+        </div>
+        <div className="dimensionbox dimensionbox4">
+          <div className="dimcontent">1200 x 2400 mm</div>
+        </div>
+        <div className="dimensionbox dimensionbox5">
+          <div className="dimcontent">1600 x 1600 mm</div>
+        </div>
+        <div className="dimensionbox dimensionbox6">
+          <div className="dimcontent">1600 x 3200 mm</div>
+        </div>
+        <div className="dimensionbox dimensionbox7">
+          <div className="dimcontent">1600 x 4800 mm</div>
+        </div>
+      </div>
+    </div>
+  );
 }
