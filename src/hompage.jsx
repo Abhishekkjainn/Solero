348164dcfb52b26dc1bb3460a3115aa04342aad1
abhishekkjainn <jainabhishek1904@gmail.com>
2024-06-13 11:24:38 +0530

    Added the menu bar and its responsiveness

--- src/hompage.jsx
+++ src/hompage.jsx
@@ -7,6 +7,7 @@ import DownloadsPage from './pages/downloadspage';
 import MediaPage from './pages/mediapage';
 import CareerPage from './pages/careerpage';
 import ContactsPage from './pages/contactspage';
+import { useState } from 'react';
 
 export default function Homepage() {
   return (
@@ -28,6 +29,16 @@ export default function Homepage() {
   );
 
   function Header() {
+    const [isMenuVisible, setIsMenuVisible] = useState(false);
+
+    const toggleMenu = () => {
+      setIsMenuVisible(!isMenuVisible);
+    };
+
+    const hideMenu = () => {
+      setIsMenuVisible(false);
+    };
+
     return (
       <div className="Header">
         <Link to={'/'} className="companylogo">
@@ -57,6 +68,80 @@ export default function Homepage() {
           <Link to="/shop" className="linkdiv">
             Shop
           </Link>
+        </div>
+
+        <div className="headerlinksresponsive">
+          <div className="checkbox">
+            <label className="hamburger">
+              <input
+                type="checkbox"
+                checked={isMenuVisible}
+                onChange={toggleMenu}
+              />
+              <svg viewBox="0 0 32 32">
+                <path
+                  className="line line-top-bottom"
+                  d="M27 10 13 10C10.8 10 9 8.2 9 6 9 3.5 10.8 2 13 2 15.2 2 17 3.8 17 6L17 26C17 28.2 18.8 30 21 30 23.2 30 25 28.2 25 26 25 23.8 23.2 22 21 22L7 22"
+                ></path>
+                <path className="line" d="M7 16 27 16"></path>
+              </svg>
+            </label>
+          </div>
+          {isMenuVisible && (
+            <div className="menubox">
+              <div className="responsivelinks">
+                <Link
+                  to="/company"
+                  className="linkdivresponsive"
+                  onClick={hideMenu}
+                >
+                  Company
+                </Link>
+                <Link
+                  to="/products"
+                  className="linkdivresponsive"
+                  onClick={hideMenu}
+                >
+                  Products
+                </Link>
+                <Link
+                  to="/downloads"
+                  className="linkdivresponsive"
+                  onClick={hideMenu}
+                >
+                  Downloads
+                </Link>
+                <Link
+                  to="/media"
+                  className="linkdivresponsive"
+                  onClick={hideMenu}
+                >
+                  Media
+                </Link>
+                <Link
+                  to="/contact"
+                  className="linkdivresponsive"
+                  onClick={hideMenu}
+                >
+                  Contact
+                </Link>
+                <Link
+                  to="/career"
+                  className="linkdivresponsive"
+                  onClick={hideMenu}
+                >
+                  Career
+                </Link>
+                <Link
+                  to="/shop"
+                  className="linkdivresponsive"
+                  onClick={hideMenu}
+                >
+                  Shop
+                </Link>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     );
