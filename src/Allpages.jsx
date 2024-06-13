eb2fbc6cee5bec49a27d48bfcbaa551c77c22f64
abhishekkjainn <jainabhishek1904@gmail.com>
2024-06-13 17:26:37 +0530

    Added the Call to action page

--- src/Allpages.jsx
+++ src/Allpages.jsx
@@ -2,6 +2,8 @@ import Page1 from './pages/Page1';
 import Page2 from './pages/Page2';
 import Page3 from './pages/Page3';
 import Page4 from './pages/Page4';
+import Page5 from './pages/Page5';
+import Page6 from './pages/Page6';
 
 export default function AllPages() {
   return (
@@ -10,6 +12,8 @@ export default function AllPages() {
       <Page2 />
       <Page3 />
       <Page4 />
+      <Page5 />
+      <Page6 />
     </>
   );
 }
---
+++ src/pages/Page5.jsx
@@ -0,0 +1,21 @@
+export default function Page5() {
+  return (
+    <div className="page5 page">
+      <div className="contentpage5">
+        Discover an extensive selection of tiles from India's largest porcelain
+        tile manufacturer, and transform your spaces to be as captivating as the
+        sky and as tranquil as nature. Solero was founded with the vision to
+        craft engaging and meaningful experiences for customers worldwide,
+        setting new standards of excellence. Each tile is meticulously designed,
+        styled, and textured using the finest materials, blending our
+        creativity, expertise, and cutting-edge technology to produce the
+        world's best porcelain slab tiles. Select your preferred style from our
+        sophisticated marble series that exudes a grand aura, the natural stone
+        series that brings nature's tranquility indoors, the wooden series for
+        an elegant rustic charm, and many more options to meet all your decor
+        needs.
+      </div>
+      <div className="calltag">Call Us At - +91 9301303584</div>
+    </div>
+  );
+}
