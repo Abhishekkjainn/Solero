f84dba75e8949e8314475485b5dafb51082c0ad2
abhishekkjainn <jainabhishek1904@gmail.com>
2024-06-13 16:05:22 +0530

    Created the Products page with hover effect over it

--- src/Allpages.jsx
+++ src/Allpages.jsx
@@ -1,11 +1,13 @@
 import Page1 from './pages/Page1';
 import Page2 from './pages/Page2';
+import Page3 from './pages/Page3';
 
 export default function AllPages() {
   return (
     <>
       <Page1 />
       <Page2 />
+      <Page3 />
     </>
   );
 }
---
+++ src/pages/Page3.jsx
@@ -0,0 +1,56 @@
+export default function Page3() {
+  return (
+    <div className="page page3">
+      <div className="headingpage3">Explore Products </div>
+      <div className="productpage">
+        <div className="line1">
+          <div className="productcard">
+            <div className="prodimage">
+              <img src="oniceniroprev.jpg" alt="" className="prodimg" />
+              <div className="hover-content">
+                <div className="prodcat">Marble</div>
+                <div className="prodname">Onice Niro</div>
+              </div>
+            </div>
+          </div>
+          <div className="productcard">
+            <div className="prodimage">
+              <img src="floorprev.jpg" alt="" className="prodimg" />
+
+              <div className="hover-content">
+                <div className="prodcat">Wood</div>
+                <div className="prodname">Wooden Flooring</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="line2">
+          <div className="productcard">
+            <div className="prodimage">
+              <img
+                src="terralianomellagranoprev.jpg"
+                alt=""
+                className="prodimg"
+              />
+              <div className="hover-content">
+                <div className="prodcat">Concrete</div>
+                <div className="prodname">Red Flooring</div>
+              </div>
+            </div>
+          </div>
+          <div className="productcard">
+            <div className="prodimage">
+              <img src="bathroomprev.jpg" alt="" className="prodimg" />
+
+              <div className="hover-content">
+                <div className="prodcat">Stone</div>
+                <div className="prodname">Textured Tile</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="exploremorebutton">Explore More</div>
+    </div>
+  );
+}
