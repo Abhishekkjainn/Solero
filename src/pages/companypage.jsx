2e32a189cb7d589d194ced8176a4b0eceac78e04
abhishekkjainn <jainabhishek1904@gmail.com>
2024-06-14 16:39:23 +0530

    Completed the Company page

--- src/pages/companypage.jsx
+++ src/pages/companypage.jsx
@@ -1,5 +1,6 @@
 import '../App.css';
 import { Parallax, Background } from 'react-parallax';
+import Page7 from './Page7';
 
 export default function CompanyPage() {
   return (
@@ -9,6 +10,8 @@ export default function CompanyPage() {
       <CompPage3 />
       <CompPage4 />
       <CompPage5 />
+      <CompPage6 />
+      <Page7 />
     </div>
   );
 
@@ -136,6 +139,65 @@ export default function CompanyPage() {
             make us your reliable partners as dynamic slab manufacturers. Our
             passion, hard work, and commitment have established us as a
             dependable tile company for your architectural needs!
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  function CompPage6() {
+    return (
+      <div className="companypage6">
+        <div className="line"></div>
+        <div className="comppage2heading">
+          {' '}
+          <div className="circle"></div> &nbsp; OUR VALUES
+        </div>
+
+        <div className="valuescontent">
+          With over 450 employees, we are a pioneering porcelain tile
+          manufacturing company that upholds values of integrity, respect,
+          creativity, innovation, and professionalism. These principles are
+          driven by our vision to develop and provide exceptional tiling
+          solutions for our clients. At Solero, our people are the backbone of
+          our business. We believe in the strength of unity and continually
+          empower each other to serve our customers, partners, and community in
+          the best possible way. We are a company of bold enthusiasts, eager to
+          approach things uniquely.
+        </div>
+
+        <div className="horizontalscrollcard">
+          <div className="trustcard trustcardcomp1">
+            <div className="heading">Integrity</div>
+            <div className="contentpg4">
+              We say what we do and we do what we say. Integrity stands at the
+              heart of what we do. By treating everyone equally, we have open
+              and honest communication. Our every action is united with fairness
+              and respect for clients and colleagues too. We believe in
+              providing the best tiling solutions- together.
+            </div>
+          </div>
+          <div className="trustcard trustcardcomp2">
+            <div className="heading">Innovation</div>
+            <div className="contentpg4">
+              Innovation leads to action. Our company constantly strives to
+              exceed the set limits through new ideas, its application, and
+              learning from mistakes. Our curiosity and dedication to creating
+              better solutions lead us to generate new tiling solutions and set
+              new benchmarks within the company as well as in the whole tile
+              industry.
+            </div>
+          </div>
+          <div className="trustcard trustcardcomp3">
+            <div className="heading">Professionalism</div>
+            <div className="contentpg4">
+              Lioli consists of experienced professionals who hold the highest
+              standards of professionalism in all roles and areas of
+              specialization. We take our work seriously! By constantly evolving
+              and upgrading our skills, we respond to the changing world by
+              going the extra mile to get the desired results. Our teamwork and
+              innovative excellence make us apart from others.
+            </div>
           </div>
         </div>
       </div>
