ac168036750b4d09fbe1889c1de65594e3e0de8f
abhishekkjainn <jainabhishek1904@gmail.com>
2024-06-18 00:52:12 +0530

    Added the downloads page

--- src/pages/downloadspage.jsx
+++ src/pages/downloadspage.jsx
@@ -7,13 +7,55 @@ export default function DownloadsPage() {
         <title>Download Center | Solero Ceramics</title>
       </Helmet>
       <div className="downloadpage">
-        <div className="downloadpage1">
-          <div className="companypageheadingpage1">
-            {' '}
-            <div className="circle comppagewhitecircle"></div> &nbsp; DOWNLOADS
-          </div>
-        </div>
+        <Downpage1 />
+        <Downpage2 />
       </div>
     </>
   );
+
+  function Downpage1() {
+    return (
+      <div className="downloadpage1">
+        <div className="companypageheadingpage1">
+          {' '}
+          <div className="circle comppagewhitecircle"></div> &nbsp; DOWNLOADS
+        </div>
+      </div>
+    );
+  }
+
+  function Downloadsec({ sno, cardheadline, doclink }) {
+    return (
+      <div className="downloadsection">
+        <div className="downpagesectioncontent">
+          {' '}
+          <div className="sno">{sno}</div>
+          <div className="headingdownpage">{cardheadline}</div>
+        </div>
+        <div className="downloadbutton"></div>
+      </div>
+    );
+  }
+
+  function Downpage2() {
+    return (
+      <div className="downpage2">
+        <Downloadsec
+          sno={'1.'}
+          cardheadline={'Product Catalog'}
+          doclink={'https://abhishekjain.vercel.app'}
+        />
+        <Downloadsec
+          sno={'2.'}
+          cardheadline={'Dimension Catalog'}
+          doclink={'https://abhishekjain.vercel.app'}
+        />
+        <Downloadsec
+          sno={'3.'}
+          cardheadline={'Company History'}
+          doclink={'https://abhishekjain.vercel.app'}
+        />
+      </div>
+    );
+  }
 }
