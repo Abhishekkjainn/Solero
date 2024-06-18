bfcb686741206c69432f36194f7c3abcd941d974
abhishekkjainn <jainabhishek1904@gmail.com>
2024-06-18 15:07:33 +0530

    Completed Download page

--- src/pages/downloadspage.jsx
+++ src/pages/downloadspage.jsx
@@ -25,6 +25,9 @@ export default function DownloadsPage() {
   }
 
   function Downloadsec({ sno, cardheadline, doclink }) {
+    const handleDownload = () => {
+      window.open(doclink, '_blank'); // Opens the PDF in a new tab
+    };
     return (
       <div className="downloadsection">
         <div className="downpagesectioncontent">
@@ -32,30 +35,59 @@ export default function DownloadsPage() {
           <div className="sno">{sno}</div>
           <div className="headingdownpage">{cardheadline}</div>
         </div>
-        <div className="downloadbutton"></div>
+        <div className="downloadbutton">
+          <button class="Btn" onClick={handleDownload}>
+            <svg
+              class="svgIcon"
+              viewBox="0 0 384 512"
+              height="1em"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M169.4 470.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 370.8 224 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 306.7L54.6 265.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"></path>
+            </svg>
+            <span class="icon2"></span>
+          </button>
+        </div>
       </div>
     );
   }
 
   function Downpage2() {
     return (
-      <div className="downpage2">
-        <Downloadsec
-          sno={'1.'}
-          cardheadline={'Product Catalog'}
-          doclink={'https://abhishekjain.vercel.app'}
-        />
-        <Downloadsec
-          sno={'2.'}
-          cardheadline={'Dimension Catalog'}
-          doclink={'https://abhishekjain.vercel.app'}
-        />
-        <Downloadsec
-          sno={'3.'}
-          cardheadline={'Company History'}
-          doclink={'https://abhishekjain.vercel.app'}
-        />
-      </div>
+      <>
+        <div className="downpage2">
+          <Downloadsec
+            sno={'1.'}
+            cardheadline={'Product Catalog'}
+            doclink={'pdf1.pdf'}
+          />
+          <Downloadsec
+            sno={'2.'}
+            cardheadline={'Dimension Catalog'}
+            doclink={'pdf1.pdf'}
+          />
+          <Downloadsec
+            sno={'3.'}
+            cardheadline={'Company History'}
+            doclink={'pdf1.pdf'}
+          />
+          <Downloadsec
+            sno={'1.'}
+            cardheadline={'Product Catalog'}
+            doclink={'pdf1.pdf'}
+          />
+          <Downloadsec
+            sno={'2.'}
+            cardheadline={'Dimension Catalog'}
+            doclink={'pdf1.pdf'}
+          />
+          <Downloadsec
+            sno={'3.'}
+            cardheadline={'Company History'}
+            doclink={'pdf1.pdf'}
+          />
+        </div>
+      </>
     );
   }
 }
